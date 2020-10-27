67c4d1cb9bcabffc70d5040869b9ed90b52d2cfc
Nick Schenone <nschenone@gmail.com>
2020-10-27 23:22:48 +0000

    Working on Camera Table

--- project/workflow.py
+++ project/workflow.py
@@ -16,98 +16,21 @@ def init_functions(functions: dict, project=None, secrets=None):
     description="Real-time facial recognition using Iguazio Data Science Platform, Nuclio, and MLRun"
 )
 def kfpipeline():
-    
-    # Custom docker image with cv2 and grafwiz
-#     image = f"docker-registry.{os.getenv('IGZ_NAMESPACE_DOMAIN')}:80/{os.getenv('DOCKER_IMAGE')}"
+    # Note: Using env/params to order components as everything has been defined in environment variables.
+    # "env" is the input for nuclio functions which output a function "endpoint"
+    # "params" is the input for jobs which output a "run_id"
 
     # Create streams and tables
-    setup = funcs['create-streams-tables'].as_step(outputs=['raw_video_stream_url', 'tagged_video_stream_url'])
+    create_streams_tables = funcs['create-streams-tables'].as_step()
     
     # Deploy facial recognition
-    # Note: image specified in function yaml due to nuclio syntax differences
-    deploy = funcs['deploy-facial-recognition'].deploy_step(env={'TAGGED_VIDEO_STREAM_URL' : setup.outputs['tagged_video_stream_url']})
+    facial_recognition = funcs['deploy-facial-recognition'].deploy_step(env={'RUN_ORDER' : create_streams_tables.outputs['run_id']})
 
-    
-#     # Create data bunches
-#     bunches = funcs['create-data-bunches'].as_step(
-#         name="create-data-bunches",
-#         handler='create_data_bunches',
-#         inputs={'data_path': ingest.outputs['data'], 'split' : split},
-#         outputs=['data_lm', 'data_clas'],
-#         image=image)
-    
-#     # Language model Hyperparameters
-#     hyperparams = {"bs" : hyper_lm_bs,
-#                    "drop_mult" : hyper_lm_drop_mult}
-    
-#     params = {"epochs" : hyper_lm_epochs,
-#               "num_samples" : data_size,
-#               "data_lm_path" : bunches.outputs['data_lm']}
-    
-#     # Language model Hyperparameter tuning
-#     hyper_tune_lm = funcs['hyper-lm'].as_step(
-#         name="hyper-lm",
-#         handler='train_lm_model',
-#         params=params,
-#         hyperparams=hyperparams,
-#         selector='max.accuracy',
-#         outputs=['best_params'],
-#         image=image)
-    
-#     # Language model training
-#     train_lm = funcs['train-lm'].as_step(
-#         name="train-lm",
-#         handler='train_lm',
-#         inputs={'train_lm_epochs': train_lm_epochs,
-#                 'data_lm_path' : bunches.outputs['data_lm'],
-#                 'num_samples' : data_size,
-#                 'hyper_lm_best_params_path' : hyper_tune_lm.outputs['best_params']},
-#         outputs=['train_lm_model', 'train_lm_model_enc', 'train_lm_accuracy'],
-#         image=image)
-    
-#     # Classification model Hyperparameters
-#     hyperparams = {"bs" : hyper_clas_bs,
-#                    "thresh" : hyper_clas_thresh,
-#                    "drop_mult" : hyper_clas_drop_mult}
-    
-#     params = {"epochs" : hyper_clas_epochs,
-#               "num_samples" : data_size,
-#               "encodings" : train_lm.outputs['train_lm_model_enc'],
-#               "data_clas_path" : bunches.outputs['data_clas']}
-    
-#     # Classification model Hyperparameter tuning
-#     hyper_tune_clas = funcs['hyper-clas'].as_step(
-#         name="hyper-clas",
-#         handler='train_clas_model',
-#         params=params,
-#         hyperparams=hyperparams,
-#         selector='max.fbeta',
-#         outputs=['best_params'],
-#         image=image)
-    
-#     # Classification model training
-#     train_clas = funcs['train-clas'].as_step(
-#         name="train-clas",
-#         handler='train_clas',
-#         inputs={'train_clas_epochs': train_clas_epochs,
-#                 'data_clas_path' : bunches.outputs['data_clas'],
-#                 'num_samples' : data_size,
-#                 'encodings' : train_lm.outputs['train_lm_model_enc'],
-#                 'hyper_clas_best_params_path' : hyper_tune_clas.outputs['best_params']},
-#         outputs=['train_clas_model', 'train_clas_fbeta'],
-#         image=image)
+    # Deploy image retrieval
+    image_retrieval = funcs['deploy-image-retrieval'].deploy_step(env={'RUN_ORDER' : facial_recognition.outputs['endpoint']})
 
-#     # Serve model
-#     deploy = funcs['model-server'].deploy_step(env={'DATA_CLAS_PATH' : bunches.outputs['data_clas'],
-#                                                    'MODEL_PATH' : train_clas.outputs['train_clas_model'],
-#                                                    f'SERVING_MODEL_{model_endpoint_name}': train_clas.outputs['train_clas_model'],
-#                                                    'NUM_PREDS' : num_preds})
-
-#     # Model serving tester
-#     tester = funcs['model-server-tester'].as_step(
-#         name='model-tester',
-#         inputs={'model_endpoint': deploy.outputs['endpoint'],
-#                 'model_name' : model_endpoint_name,
-#                 'data_size' : data_size,
-#                 'data_path' : ingest.outputs['data'],
-#                 'num_tests' : num_tests})
+    # Create API gateway
+    api_gateway = funcs['create-api-gateway'].as_step(params={'RUN_ORDER' : image_retrieval.outputs['endpoint']})
+    
+    # Create Grafana dashboard
+    api_gateway = funcs['create-grafana-dashboard'].as_step(params={'RUN_ORDER' : api_gateway.outputs['run_id']})
