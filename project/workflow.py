3b805cfea23932851fff4373d401e29c30b88c20
Nick Schenone <nschenone@gmail.com>
2020-10-30 15:57:28 +0000

    Working on removing YAML file deployment

--- project/workflow.py
+++ project/workflow.py
@@ -24,15 +24,15 @@ def kfpipeline():
     create_streams_tables = funcs['create-streams-tables'].as_step(outputs=['tagged_video_stream_url', 
                                                                             'raw_video_stream_url'])
     
-    # Deploy facial recognition
-    facial_recognition = funcs['deploy-facial-recognition'].deploy_step(env={'RUN_ORDER' : create_streams_tables.outputs['run_id'],
-                                                                             'TAGGED_VIDEO_STREAM_URL' : create_streams_tables.outputs['tagged_video_stream_url']})
+#     # Deploy facial recognition
+#     facial_recognition = funcs['deploy-facial-recognition'].deploy_step(env={'RUN_ORDER' : create_streams_tables.outputs['run_id'],
+#                                                                              'TAGGED_VIDEO_STREAM_URL' : create_streams_tables.outputs['tagged_video_stream_url']})
 
-    # Deploy image retrieval
-    image_retrieval = funcs['deploy-image-retrieval'].deploy_step(env={'RUN_ORDER' : facial_recognition.outputs['endpoint']})
+#     # Deploy image retrieval
+#     image_retrieval = funcs['deploy-image-retrieval'].deploy_step(env={'RUN_ORDER' : facial_recognition.outputs['endpoint']})
 
-    # Create API gateway
-    api_gateway = funcs['create-api-gateway'].as_step(params={'RUN_ORDER' : image_retrieval.outputs['endpoint']})
+#     # Create API gateway
+#     api_gateway = funcs['create-api-gateway'].as_step(params={'RUN_ORDER' : image_retrieval.outputs['endpoint']})
     
-    # Create Grafana dashboard
-    api_gateway = funcs['create-grafana-dashboard'].as_step(params={'RUN_ORDER' : api_gateway.outputs['run_id']})
+#     # Create Grafana dashboard
+#     api_gateway = funcs['create-grafana-dashboard'].as_step(params={'RUN_ORDER' : api_gateway.outputs['run_id']})
