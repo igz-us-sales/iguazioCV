ae1506d86d7de3bd61b14dc6bf22c961833287fb
Nick Schenone <nschenone16@gmail.com>
2020-11-06 00:08:03 +0000

    Packaged client scripts

--- components/client/video_capture.py
+++ components/client/video_capture.py
@@ -14,33 +14,17 @@ from cv2 import VideoWriter, VideoWriter_fourcc
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-os.environ["V3IO_ACCESS_KEY"] = "XXXXXXXXXXXX"
-os.environ["V3IO_USERNAME"] = "XXXX"
-os.environ[
-    "V3IO_WEBAPI"
-] = "https://webapi.default-tenant.app.XXXXXXXXXXX.com"
-os.environ[
-    "V3IO_FRAMES"
-] = "https://framesd.default-tenant.app.XXXXXXXX.com"
-os.environ["IGZ_CONTAINER"] = "bigdata"
-os.environ["RAW_VIDEO_STREAM"] = "videostream"
-os.environ["CAMERA_LIST_TBL"] = "camera_list"
-os.environ["shardId"] = "0"
-os.environ["cameraID"] = "0"
-os.environ["cameraURL"] = "http://XXX.XXX.0.XXX:XXXX"
+import yaml
+with open("client_config.yaml") as f:
+    config = yaml.safe_load(f)
 
+url = f"{config['v3io']['webapi']}/{config['project']['container']}/{config['stream']['raw_video_stream']}/"    
 
-url = "%s/%s/%s/" % (
-    os.getenv("V3IO_WEBAPI"),
-    os.getenv("IGZ_CONTAINER"),
-    os.getenv("RAW_VIDEO_STREAM"),
-)
 headers = {
     "Content-Type": "application/json",
     "X-v3io-function": "PutRecords",
-    "X-v3io-session-key": os.getenv("V3IO_ACCESS_KEY"),
+    "X-v3io-session-key": config['v3io']['access_key']
 }
-
 
 def stream_frame_write(cameraID, payload):
     print("stream_frame_write called")
@@ -51,11 +35,11 @@ def stream_frame_write(cameraID, payload):
     return r.text
 
 
-def start_capture(cameraID: str, cameraURL: str, shard: int):
+def start_capture():
     print("start capture")
 
     # To capture video from webcam
-    cap = cv2.VideoCapture(cameraURL)
+    cap = cv2.VideoCapture(config['camera']['url'])
 
     # To use a video file as input
     # cap = cv2.VideoCapture('filename.mp4')
@@ -70,12 +54,12 @@ def start_capture(cameraID: str, cameraURL: str, shard: int):
             ret, img = cap.read()
             ret, buffer = cv2.imencode(".jpg", img)
             data = base64.b64encode(buffer)
-            Records.append({"Data": data.decode("utf-8"), "ShardId": shard})
+            Records.append({"Data": data.decode("utf-8"), "ShardId": config['stream']['shard_id']})
 
             if data_count == 60:
                 try:
                     payload = {"Records": Records}
-                    r = stream_frame_write(cameraID, payload)
+                    r = stream_frame_write(config['camera']['id'], payload)
                     print(r)
                 except:
                     print("Failed to write to shard %s" % shard)
@@ -88,15 +72,15 @@ def start_capture(cameraID: str, cameraURL: str, shard: int):
 
 
 def get_cameras_list():
-    client = v3f.Client(os.getenv("V3IO_FRAMES"), container=os.getenv("IGZ_CONTAINER"))
-    df = client.read("kv", os.getenv("CAMERA_LIST_TBL"))
+    client = v3f.Client(config['v3io']['frames'], container=config['project']['container'])
+    df = client.read("kv", config['camera']['list_table'])
     return df
 
 
 def init_function():
-    cameraID = os.getenv("cameraID")
-    shardId = int(os.getenv("shardId"))
-    cameraURL = os.getenv("cameraURL")
+    cameraID = config['camera']['id']
+    shardId = int(config['stream']['shard_id'])
+    cameraURL = config['camera']['url']
 
     if isinstance(cameraURL, int):
         cameraURL = int(cameraURL)
@@ -110,31 +94,8 @@ def init_function():
             and row["url"] == cameraURL
             and row["active"] == True
         ):
-            start_capture(cameraID, cameraURL, shardId)
+            start_capture()
     print("Invalid camera")
 
 
 init_function()
-
-
-# Variables needed for container operations
-#
-# V3IO_ACCESS_KEY
-#
-# V3IO_USERNAME
-#
-# V3IO_WEBAPI
-#
-# V3IO_FRAMES
-#
-# IGZ_CONTAINER
-#
-# RAW_VIDEO_STREAM
-#
-# CAMERA_LIST_TBL
-#
-# shardId
-#
-# cameraID
-#
-# cameraURL
